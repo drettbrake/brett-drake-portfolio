af217956cad6773b5b0e7301a38a21b9825ef7f3
Brett Drake <drakebr@umich.edu>
2021-06-26 15:30:39 -0400

    initial commit

---
+++ src/app/components/nav-toolbar/nav-toolbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nav-toolbar',
+  templateUrl: './nav-toolbar.component.html',
+  styleUrls: ['./nav-toolbar.component.css']
+})
+export class AppComponent {
+  
+}
